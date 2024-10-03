0c5da559f5282c57346696eb8aad43be0d068c29
Mitchel Soto <daronmitchel@gmail.com>
2024-10-03 20:17:26 +0000

    Changed port from 8080 to 5050.

--- sample_app.py
+++ sample_app.py
@@ -1,13 +1,16 @@
-# Add to this file for the sample app lab
 from flask import Flask
 from flask import request
 from flask import render_template
 
+# Crear la instancia de Flask
 sample = Flask(__name__)
 
+# Definir la ruta principal
 @sample.route("/")
 def main():
     return render_template("index.html")
 
+# No es necesario el bloque para ejecutar Flask directamente
+# ya que Gunicorn se encargará de ejecutar la aplicación.
 if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=5050)
+    pass  # No se necesita ejecutar Flask directamente aquí
